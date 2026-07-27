765c69898bef3c9be79e79154174fcae60935371
popnp <73061674+popnp@users.noreply.github.com>
2026-07-27 20:51:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 */
 
 /* Bump this on every release. Changing it is what retires the old cache. */
-const VERSION = '2026-07-28f';
+const VERSION = '2026-07-28h';
 const CACHE = 'popnp-' + VERSION;
 
 const ASSETS = [
